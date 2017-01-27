e9f4e0ae9bcb1cf5f714b98eab54c95d72ce3440
MelofunkCodes <myeoh27@gmail.com>
2017-01-27 16:56:51 +0000

    Updated reddit.js and demo....js to promises version and updated getAllPosts inside reddit API

--- demo-using-api.js
+++ demo-using-api.js
@@ -1,38 +1,74 @@
-// load the mysql library
-var mysql = require('mysql');
+// load the mysqlPromise library
+var mysql = require('promise-mysql');
 
 // create a connection to our Cloud9 server
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'ziad_saab', // CHANGE THIS :)
-  password : '',
-  database: 'reddit'
+var connection = mysql.createPool({
+    host: 'localhost',
+    user: 'myeoh27',
+    password: '',
+    database: 'reddit',
+    connectionLimit: 10
 });
 
 // load our API and pass it the connection
 var reddit = require('./reddit');
-var redditAPI = reddit(connection);
+var redditAPI = reddit(connection); 
 
+
+//==========CODE EXECUTION=======================
 // It's request time!
 redditAPI.createUser({
-  username: 'hello23',
-  password: 'xxx'
-}, function(err, user) {
-  if (err) {
-    console.log(err);
-  }
-  else {
-    redditAPI.createPost({
-      title: 'hi reddit!',
-      url: 'https://www.reddit.com',
-      userId: user.id
-    }, function(err, post) {
-      if (err) {
-        console.log(err);
-      }
-      else {
-        console.log(post);
-      }
-    });
-  }
+  username: 'applesFOlyfe',
+  password: 'derp'
+})
+.then(function(user){
+    console.log(user); //testing to see if user created
+  
+    return redditAPI.createPost({
+        title: 'Waddup world',
+        url: 'https://www.reddit.com/',
+        userId: user.id
+      });
+})
+.then(function(post){
+  console.log(post);
+  connection.end();
+})
+.catch(function(error){
+    console.log("Error happened", error);
+  
+  connection.end();
+
 });
+
+
+// //creating posts with existing users
+// redditAPI.createPost({
+//   title: 'Tuxedo on Tour! 2017 dates released!',
+//   url: 'http://www.mayerhawthorne.com/tuxedo-on-tour/',
+//   userId: 3
+// })
+// .then(function(post){
+//   console.log(post);
+//   connection.end();
+// })
+// .catch(function(error){
+//     console.log("Error happened", error);
+  
+//   connection.end();
+
+// });
+
+
+// //displaying all posts
+// redditAPI.getAllPosts()
+// .then(function(bigPostsTable){
+//   console.log(bigPostsTable);
+//   connection.end();
+// })
+// .catch(function(error){
+//     console.log("Error happened", error);
+  
+//   connection.end();
+
+// });
--- reddit.js
+++ reddit.js
@@ -1,114 +1,124 @@
 var bcrypt = require('bcrypt');
-var HASH_ROUNDS = 10;
+const saltRounds = 10;
+
+/*
+QUESTIONS:
+1. demo-using-api.js - line 14. 
+1A: This allows us pass a connection to however many different API "files" we use vs copy+pasting to top of every file
+2. what is insertId at line 43 and 80?? 
+2A: insertId is just a property included in whatever result that you have from a mysql query?
+3. in getAllPosts, in what event/case would a callback not exist?
+
+testing
+4. Why is my user id skipping when I've tested the throwing error on duplicate case, and then creating a new successful user? i.e. I jumped from id 4 to 6.
+
+*/
 
 module.exports = function RedditAPI(conn) {
-  return {
-    createUser: function(user, callback) {
-      
-      // first we have to hash the password...
-      bcrypt.hash(user.password, HASH_ROUNDS, function(err, hashedPassword) {
-        if (err) {
-          callback(err);
-        }
-        else {
-          conn.query(
-            'INSERT INTO users (username,password, createdAt) VALUES (?, ?, ?)', [user.username, hashedPassword, new Date()],
-            function(err, result) {
-              if (err) {
-                /*
-                There can be many reasons why a MySQL query could fail. While many of
-                them are unknown, there's a particular error about unique usernames
-                which we can be more explicit about!
-                */
-                if (err.code === 'ER_DUP_ENTRY') {
-                  callback(new Error('A user with this username already exists'));
-                }
-                else {
-                  callback(err);
-                }
-              }
-              else {
-                /*
-                Here we are INSERTing data, so the only useful thing we get back
-                is the ID of the newly inserted row. Let's use it to find the user
-                and return it
-                */
-                conn.query(
-                  'SELECT id, username, createdAt, updatedAt FROM users WHERE id = ?', [result.insertId],
-                  function(err, result) {
-                    if (err) {
-                      callback(err);
-                    }
-                    else {
-                      /*
-                      Finally! Here's what we did so far:
-                      1. Hash the user's password
-                      2. Insert the user in the DB
-                      3a. If the insert fails, report the error to the caller
-                      3b. If the insert succeeds, re-fetch the user from the DB
-                      4. If the re-fetch succeeds, return the object to the caller
-                      */
-                        callback(null, result[0]);
-                    }
-                  }
-                );
-              }
+    return {
+      createUser: function(user) {
+
+        // first we have to hash the password...
+        return bcrypt.hash(user.password, saltRounds)
+          .then(function(hashedPassword) {
+            //console.log(hashedPassword)
+            return conn.query(
+              'INSERT INTO users (username, password, createdAt) VALUES (?, ?, ?)', [user.username, hashedPassword, new Date()] );
+          })
+          .catch(function(err) {
+            if (err.code === 'ER_DUP_ENTRY') {
+              throw new Error('A user with this username already exists');
             }
-          );
+            console.log(err);
+          })
+          .then(function(newUser) {
+
+            return conn.query(
+              'SELECT id, username, createdAt, updatedAt FROM users WHERE id = ?', [newUser.insertId] );
+
+          })
+          .then(function(userTable) {
+            return userTable[0];
+          });
+
+      },
+      createPost: function(post) {
+        return conn.query(
+            'INSERT INTO posts (userId, title, url, createdAt) VALUES (?, ?, ?, ?)', [post.userId, post.title, post.url, new Date()])
+          .then(function(newPost) {
+            return conn.query(
+                'SELECT id,title,url,userId, createdAt, updatedAt FROM posts WHERE id = ?', [newPost.insertId]);
+          })
+          .then(function(postTable) {
+            return postTable[0];
+          });
+      },
+      getAllPosts: function(options) {
+        
+        //console.log("options: ", options);
+        if (!options){
+          options = {}; //create empty object if no argument passed to getAllPosts
         }
-      });
-    },
-    createPost: function(post, callback) {
-      conn.query(
-        'INSERT INTO posts (userId, title, url, createdAt) VALUES (?, ?, ?, ?)', [post.userId, post.title, post.url, new Date()],
-        function(err, result) {
-          if (err) {
-            callback(err);
-          }
-          else {
-            /*
-            Post inserted successfully. Let's use the result.insertId to retrieve
-            the post and send it to the caller!
-            */
-            conn.query(
-              'SELECT id,title,url,userId, createdAt, updatedAt FROM posts WHERE id = ?', [result.insertId],
-              function(err, result) {
-                if (err) {
-                  callback(err);
+          
+          //if no "options" is sent to getAllPosts, limit = 25, and offset = 0 (first page)
+          var limit = options.numPerPage || 25; 
+          var offset = (options.page || 0) * limit; // How to flip through your table, looking at it in groups of 25 rows. THIS ALLOWS FOR PAGINATION OF OUR POSTS.
+          
+          //console.log("limit: ", limit);
+          //console.log("offset: ", offset);
+
+          return conn.query(`
+                  SELECT 
+                      posts.id AS postId,
+                      posts.title,
+                      posts.url,
+                      posts.userId,
+                      posts.createdAt AS postCreatedAt,
+                      posts.updatedAt AS postUpdatedAt,
+                      users.id,
+                      users.username,
+                      users.createdAt,
+                      users.updatedAt
+                  FROM posts
+                      JOIN users ON posts.userId = users.id
+                      ORDER BY postCreatedAt DESC
+                      LIMIT ? OFFSET ?`, [limit, offset])
+            .then(function(bigPostsTable) {
+              /*
+              LOGIC
+              1. Creat empty array, var posts.
+              2. Use forEach to loop through bigPostsTable
+              3. at eachPost, create an object, with user properties NESTED into another object
+              4. push eachPost into posts array
+              5. may have to return JSON.stringify(posts, null ,4);
+              
+              */
+              
+              var posts = [];
+              
+              bigPostsTable.forEach(function(eachPost){
+                var post = {
+                  id: eachPost.postId,
+                  title: eachPost.title,
+                  url: eachPost.url,
+                  createdAt: eachPost.postCreatedAt,
+                  updatedAt: eachPost.postUpdatedAt,
+                  userId: eachPost.userId,
+                  user: {
+                    id: eachPost.id,
+                    username: eachPost.username,
+                    createdAt: eachPost.createdAt,
+                    updatedAt: eachPost.updatedAt
+                  }
                 }
-                else {
-                  callback(null, result[0]);
-                }
-              }
-            );
-          }
-        }
-      );
-    },
-    getAllPosts: function(options, callback) {
-      // In case we are called without an options parameter, shift all the parameters manually
-      if (!callback) {
-        callback = options;
-        options = {};
-      }
-      var limit = options.numPerPage || 25; // if options.numPerPage is "falsy" then use 25
-      var offset = (options.page || 0) * limit;
-      
-      conn.query(`
-        SELECT id, title, url, userId, createdAt, updatedAt
-        FROM posts
-        ORDER BY createdAt DESC
-        LIMIT ? OFFSET ?`
-        , [limit, offset],
-        function(err, results) {
-          if (err) {
-            callback(err);
-          }
-          else {
-            callback(null, results);
-          }
-        }
-      );
-    }
-  }
-}
+                
+                posts.push(post);
+              });
+              
+              
+              return posts;
+            });
+
+        } //closing bracket for getAllPosts
+    } //closing bracket for BIG return at line 14
+  } //closing bracket for line 13
